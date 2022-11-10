2a9947a022e20a5aa29e04a73c46c74137837265
Matrix <rhailu88@gmail.com>
2022-11-10 17:55:25 -0500

    Added parameters for password to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,12 @@ function generatePassword() {
   alert(`Your password will have ${confirmLength} characters`);
 
   // Determine parameters of password 
+  
   var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
   var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
   var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
   var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+  
   // Loop if answer is outside the parameters 
   while (confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
     alert("You must choose at least one parameter");
